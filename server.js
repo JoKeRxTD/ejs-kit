fa516c4a606898f14fae60d2bef7d6bf7d9daea4
tunardev <tunardev@gmail.com>
2021-07-26 19:09:45 +0400

    add files

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const routes = require("./routes");
+const path = require("path");
+const ejs = require("ejs");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })
+);
+
+app.set("views", path.join(__dirname, "/pages"));
+app.engine("ejs", ejs.renderFile);
+app.set("view engine", "ejs");
+
+app.get("*", async (req, res) => {
+  fs.readdir("./pages", async (err, files) => {
+    if (err) res.status(404).json({ message: "Error: " + err });
+    if (
+      files.some(
+        (file) =>
+          file.endsWith(".ejs") &&
+          (req.originalUrl !== "/"
+            ? req.originalUrl.replace("/", "") + ".ejs" === file
+            : "index.ejs" === file)
+      )
+    ) {
+      res.render(
+        req.originalUrl !== "/"
+          ? req.originalUrl.replace("/", "") + ".ejs"
+          : "index.ejs"
+      );
+    } else {
+      return res.json({ message: "404 not found" });
+    }
+  });
+});
+
+app.use("/api", routes);
+
+app.listen(PORT, () => {
+  console.log("Server active | port: ", PORT);
+});
